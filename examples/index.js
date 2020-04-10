582637f4e16ad6b11e0cffdc56eb262f047ad152
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-04-10 12:01:46 -0500

    Finished model object

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,5 @@
+var triangle, scene;
+
 function main() {
   const canvas = document.querySelector("#canvas");
   const camera = new PerspectiveCamera(
@@ -8,10 +10,10 @@ function main() {
   );
   scene = new Scene(canvas, camera);
 
-  shader = new Shader();
+  const shader = new Shader();
 
   triangle = new Model({
-    vertices: [0, 0.5, 0, -0.5, -0.5, 0, 0.5, -0.5, 0],
+    vertices: [1, 1.5, 0, 0.5, 0, 0, 1.5, 0, 0],
     indices: [0, 1, 2],
     primitive: Constants.primitives.triangles,
     shader,
@@ -19,6 +21,19 @@ function main() {
 
   scene.addComponent(triangle);
   camera.zoom(-8);
+  //triangle.setPosition({ x: 6, y: 3, z: 0 });
+  // triangle.rotateAbout({ x: 1.3, y: 0.4 });
   camera.pan(3);
+  requestAnimationFrame(render);
+  triangle.replaceCoordinates(
+    [0, 0.5, 0, -0.5, -0.5, 0, 0.5, -0.5, 0],
+    [0, 1, 2]
+  );
   scene.render();
+}
+
+function render() {
+  scene.render();
+  triangle.setRotation(-5, [0, 0, 1]);
+  requestAnimationFrame(render);
 }
--- src/model.js
+++ src/model.js
@@ -4,76 +4,170 @@ class Model {
     this.indices = data.indices;
     this.position = [0, 0, 0];
     this.scaling = [1, 1, 1];
-    this.sQ = [0, 0, 0];
     this.rotationAxis = [0, 0, 0];
     this.rotationAngle = 0;
+    this.sQ = [0, 0, 0];
     this.rQ = [0, 0, 0];
-    this.primitive = data.primitive || Constants.primitives.points;
-    this.matrix = glMatrix.mat4.create();
+    this.primitive = data.primitive ?? Constants.primitives.points;
     this.shader = data.shader;
+    this.matrix = glMatrix.mat4.create();
   }
 
+  /**
+   * DO NOT USE DIRECTLY!
+   * Inits the model with its shader and renderer.
+   * @param {WebGLRenderingContext} gl 
+   */
   init(gl) {
     this.shader.init(gl);
     this.renderer = new Renderer(gl, this);
   }
 
   /**
+   * Replaces the model's vertices and indices.
+   * @param {Array<Number>} vertices 
+   * @param {*} indices 
+   */
+  replaceCoordinates(vertices, indices) {
+    this.vertices = vertices;
+    this.indices = indices;
+  }
+
+  /**
    * Translates the model's position by the amount of units.
-   * @param {number} xUnits - Amount of units in x axis.
-   * @param {number} yUnits - Amount of units in y axis.
-   * @param {number} zUnits - Amount of units in z axis.
+   * @param {Number} xUnits - Amount of units in x axis.
+   * @param {Number} yUnits - Amount of units in y axis.
+   * @param {Number} zUnits - Amount of units in z axis.
    * @return {Model} The model.
    */
   translate(xUnits, yUnits = 0, zUnits = 0) {
-    this.position[0] += xUnits;
-    this.position[1] += yUnits;
-    this.position[2] += zUnits;
+    return this.setPosition({
+      x: this.position[0] + xUnits,
+      y: this.position[1] + yUnits,
+      z: this.position[2] + zUnits,
+    });
+  }
+
+  /**
+   * Sets position of the model. If a unit is missing, it uses the model's current position.
+   * @param {Object} position - Holds x, y and z values for the position.
+   * @return {Model} The model.
+   */
+  setPosition(position) {
+    this.position[0] = position.x ?? this.position[0];
+    this.position[1] = position.y ?? this.position[1];
+    this.position[2] = position.z ?? this.position[2];
     glMatrix.mat4.translate(this.matrix, this.matrix, this.position);
     return this;
   }
 
   /**
-   * Rotates the model on a specified axis by an amount of degrees.
-   * @param {number} deg - The degrees to rotate by.
-   * @param {Array.<number>} axis - The rotation axis.
+   * Increments the model's rotation on a specified axis by an amount of degrees.
+   * @param {Number} deg - The degrees to rotate by.
+   * @param {Array.<Number>} axis - The rotation axis.
    * @return {Model} The model.
    */
   rotate(deg, axis) {
-    this.rotationAngle = (this.rotationAngle + (deg * Math.PI) / 180) % 360;
+    const degRotationAngle = (this.rotationAngle * 180) / Math.PI;
+    return this.setRotation(deg + degRotationAngle, axis);
+  }
+
+  /**
+   * Sets rotation of the model on a specified axis by an amount of degrees.
+   * @param {Number} deg - The degrees to rotate by.
+   * @param {Array.<Number>} axis - The rotation axis.
+   * @return {Model} The model.
+   */
+  setRotation(deg, axis) {
+    this.rotationAngle = ((deg * Math.PI) / 180) % 360;
     this.rotationAxis[0] = axis[0];
     this.rotationAxis[1] = axis[1];
     this.rotationAxis[2] = axis[2];
+    glMatrix.mat4.translate(this.matrix, this.matrix, this.rQ);
     glMatrix.mat4.rotate(
       this.matrix,
       this.matrix,
       this.rotationAngle,
       this.rotationAxis
     );
+    glMatrix.mat4.translate(
+      this.matrix,
+      this.matrix,
+      this.rQ.map((_) => -_)
+    );
     return this;
   }
 
   /**
+   * Sets the model to rotate about a point q.
+   * @param {Object} q - Holds x, y and z values
+   */
+  rotateAbout(q) {
+    this.rQ[0] = q.x ?? 0;
+    this.rQ[1] = q.y ?? 0;
+    this.rQ[2] = q.z ?? 0;
+  }
+
+  /**
    * Scales the model by the amount of units.
-   * @param {number} xUnits - Amount of units in x axis greater than 0.
-   * @param {number} yUnits - Amount of units in y axis greater than 0.
-   * @param {number} zUnits - Amount of units in z axis greater than 0.
+   * @param {Number} xUnits - Amount of units in x axis greater than 0.
+   * @param {Number} yUnits - Amount of units in y axis greater than 0.
+   * @param {Number} zUnits - Amount of units in z axis greater than 0.
    * @return {Model} The model.
    */
   scale(xUnits, yUnits = 1, zUnits = 1) {
-    this.scaling[0] *= xUnits;
-    this.scaling[1] *= yUnits;
-    this.scaling[2] *= zUnits;
+    return this.setScaling({
+      x: this.scaling[0] * xUnits,
+      y: this.scaling[1] * yUnits,
+      z: this.scaling[2] * zUnits,
+    });
+  }
+
+  /**
+   * Sets scaling of the model by an amount of units.
+   * If a unit is missing, it uses the model's current scaling.
+   * @param {*} scaling - Holds x, y and z values for the scaling.
+   */
+  setScaling(scaling) {
+    this.scaling[0] = scaling.x ?? this.scaling[0];
+    this.scaling[1] = scaling.y ?? this.scaling[1];
+    this.scaling[2] = scaling.z ?? this.scaling[2];
+    glMatrix.mat4.translate(this.matrix, this.matrix, this.sQ);
     glMatrix.mat4.scale(this.matrix, this.matrix, this.scaling);
+    glMatrix.mat4.translate(
+      this.matrix,
+      this.matrix,
+      this.sQ.map((_) => -_)
+    );
     return this;
   }
 
+  /**
+   * DO NOT USE DIRECTLY!
+   * Sets the model to scale about a point q.
+   * @param {Object} q - Holds x, y and z values
+   */
+  scaleAbout(q) {
+    this.sQ[0] = q.x ?? 0;
+    this.sQ[1] = q.y ?? 0;
+    this.sQ[2] = q.z ?? 0;
+  }
+
+  /**
+   * DO NOT USE DIRECTLY!
+   * Updates the model's buffers and matrices.
+   * Intended to be used before the render method.
+   * @param {Camera} camera - The camera that's being used in the scene.
+   */
   update(camera) {
     if (!this.renderer)
       throw new Error("Component hasn't been initialized yet");
     this.renderer.update(this, camera);
   }
 
+  /**
+   * Render's the model in the scene.
+   */
   render() {
     if (!this.renderer)
       throw new Error("Component hasn't been initialized yet");
